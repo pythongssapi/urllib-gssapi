ee6e9c56444ca004220cae7d8c84da902eda3e2d
tolsen@limebits.com <tolsen@limebits.com>
2008-06-24 17:53:19 -0400

    add COPYING file with full GPL license

--- http_kerberos_auth_handler.py
+++ http_kerberos_auth_handler.py
@@ -13,8 +13,8 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-# A copy of the GNU General Public License can be found at
-# <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import re
 import logging
