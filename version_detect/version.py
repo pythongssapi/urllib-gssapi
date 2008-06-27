8dde6cbe98d59c3977a03f013b55b279e667e2d8
tolsen@limebits.com <tolsen@limebits.com>
2008-06-27 14:34:31 -0400

    hg shows modification after setting keyword expansion for version.py's new location

--- version_detect/version.py
+++ version_detect/version.py
@@ -1,10 +1,10 @@
 import hg_keywords
 
 keywords = hg_keywords.keywords(
-    "$branches:  $",
-    "$node: 83f8cf753f41eb3d53d3d4b95a8cc56c18b0c3fc $",
-    "$rev: 8 $",
-    "$tags: tip $"
+    "$branches$",
+    "$node$",
+    "$rev$",
+    "$tags$"
     )
 
 version = "dev_%s_%s" % tuple([keywords[x] for x in 'rev', 'node'])
